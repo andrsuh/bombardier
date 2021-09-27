40920433bb24b5a3ade84a3e462c1825a0704e02
mikhniuk <r.mikhniuk@gmail.com>
2021-09-27 22:18:44 +0300

    Review fixes

--- src/main/kotlin/com/itmo/microservices/demo/bombardier/stages/OrderDeliveryStage.kt
+++ src/main/kotlin/com/itmo/microservices/demo/bombardier/stages/OrderDeliveryStage.kt
@@ -3,6 +3,8 @@ package com.itmo.microservices.demo.bombardier.stages
 import com.itmo.microservices.demo.bombardier.flow.*
 import com.itmo.microservices.demo.bombardier.utils.ConditionAwaiter
 import java.util.concurrent.TimeUnit
+import kotlin.time.Duration
+import kotlin.time.ExperimentalTime
 
 class OrderDeliveryStage(
     private val serviceApi: ServiceApi
@@ -11,24 +13,33 @@ class OrderDeliveryStage(
         val log = CoroutineLoggingFactory.getLogger(OrderPaymentStage::class.java)
     }
 
+    @OptIn(ExperimentalTime::class)
     override suspend fun run(): TestStage.TestContinuationType {
         val orderBeforeDelivery = serviceApi.getOrder(testCtx().orderId!!)
+
+        if (orderBeforeDelivery.status !is OrderStatus.OrderPayed) {
+            log.error("Incorrect order ${orderBeforeDelivery.id} status before OrderDeliveryStage ${orderBeforeDelivery.status}")
+            return TestStage.TestContinuationType.FAIL
+        }
+
+        serviceApi.simulateDelivery(testCtx().orderId!!)
 
         if (orderBeforeDelivery.status !is OrderStatus.OrderInDelivery) {
             log.error("Incorrect order ${orderBeforeDelivery.id} status for OrderDeliveryStage ${orderBeforeDelivery.status}")
             return TestStage.TestContinuationType.FAIL
         }
 
-        serviceApi.simulateDelivery(testCtx().orderId!!)
-
-        val lastDeliveryTimeInMillis = (orderBeforeDelivery.status.deliveryStartTime + orderBeforeDelivery.deliveryDuration!!) * 1000
-        ConditionAwaiter.awaitAtMost(lastDeliveryTimeInMillis - System.currentTimeMillis(), TimeUnit.MILLISECONDS)
+        val deliveryDuration = Duration.seconds(orderBeforeDelivery.deliveryDuration!!)
+            .minus(Duration.milliseconds(System.currentTimeMillis() - orderBeforeDelivery.paymentHistory.last().timestamp))
+        ConditionAwaiter.awaitAtMost(deliveryDuration.inWholeMilliseconds, TimeUnit.MILLISECONDS)
             .condition {
                 val updatedOrder = serviceApi.getOrder(testCtx().orderId!!)
-                updatedOrder.status != orderBeforeDelivery.status || serviceApi.userFinancialHistory(
-                    testCtx().userId!!,
-                    testCtx().orderId!!
-                ).firstOrNull { it.type == FinancialOperationType.REFUND } != null
+                updatedOrder.status is OrderStatus.OrderDelivered ||
+                        updatedOrder.status is OrderStatus.OrderRefund &&
+                        serviceApi.userFinancialHistory(
+                            testCtx().userId!!,
+                            testCtx().orderId!!
+                        ).last().type == FinancialOperationType.REFUND
             }
             .onFailure {
                 log.error("Order status of order ${orderBeforeDelivery.id} not changed and no refund")
@@ -44,13 +55,20 @@ class OrderDeliveryStage(
                 }
                 log.info("Order ${orderAfterDelivery.id} was successfully delivered")
             }
-            is OrderStatus.OrderFailed -> {
+            is OrderStatus.OrderRefund -> {
                 val userFinancialHistory = serviceApi.userFinancialHistory(testCtx().userId!!, testCtx().orderId!!)
                 if (userFinancialHistory.filter { it.type == FinancialOperationType.WITHDRAW }.sumOf { it.amount } !=
                     userFinancialHistory.filter { it.type == FinancialOperationType.REFUND }.sumOf { it.amount }) {
                     log.error("Withdraw and refund amount are different for order ${orderAfterDelivery.id}, " +
-                            "withdraw = ${userFinancialHistory.filter { it.type == FinancialOperationType.WITHDRAW }.map { it.amount }.sum()}, " +
-                            "refund = ${userFinancialHistory.filter { it.type == FinancialOperationType.REFUND }.sumOf { it.amount }}")
+                            "withdraw = ${
+                                userFinancialHistory.filter { it.type == FinancialOperationType.WITHDRAW }
+                                    .sumOf { it.amount }
+                            }, " +
+                            "refund = ${
+                                userFinancialHistory.filter { it.type == FinancialOperationType.REFUND }
+                                    .sumOf { it.amount }
+                            }"
+                    )
                 }
                 log.info("Refund for order ${orderAfterDelivery.id} is correct")
             }
