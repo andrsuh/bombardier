cab36bd4b1316dd23e6e4236061928666c169f0a
Sasha Blashenkov <Sasha.Blashenkov@jetbrains.com>
2021-12-07 23:35:26 +0300

    cleanup

--- src/main/kotlin/com/itmo/microservices/demo/DemoServiceApplication.kt
+++ src/main/kotlin/com/itmo/microservices/demo/DemoServiceApplication.kt
@@ -4,7 +4,6 @@ import kotlinx.coroutines.runBlocking
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-
 @SpringBootApplication
 class DemoServiceApplication
 
